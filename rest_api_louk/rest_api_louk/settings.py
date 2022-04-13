e32bd3f2310c7f754c56f21e2b810b716df94db4
louksky <37488746+louksky@users.noreply.github.com>
2022-04-13 20:38:56 +0300

    Update settings.py

--- rest_api_louk/rest_api_louk/settings.py
+++ rest_api_louk/rest_api_louk/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qn89f&ruskx&x+-)b@*ns3g^sl6c2!v%2_7@*cd$+)2*liq_@m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
